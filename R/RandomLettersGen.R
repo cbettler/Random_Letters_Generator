da1e3034e0a2865e4ac33c0d4d6612535002ff6e
cbettler <carlee.bettler@gmail.com>
2019-11-22 21:58:28 -0600

    Add files via upload

---
+++ R/RandomLettersGen.R
@@ -0,0 +1,16 @@
+#' @title RandomLettersGenerator
+#'
+#' @description This package generates a user-specified number of random capital English
+#'
+#' @examples randomLetterFunc(5)
+#'
+#' @export
+
+randomLetterFunc <- function(n=1){
+    alphabetVec <- c()
+
+    alphabetVec <- stringi::stri_rand_strings(n, 1, '[A-Z]')
+
+    return(alphabetVec)
+}
+
